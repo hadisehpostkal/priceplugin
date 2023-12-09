80979d960551d0db17ae4555a69b34cdbc3913dc
hadisehpostkal <88317413+hadisehpostkal@users.noreply.github.com>
2023-12-09 13:47:44 +0100

    Update PricePlugin.php

--- PricePlugin.php
+++ PricePlugin.php
@@ -10,50 +10,53 @@
  */
 
 
-function pluginprefix_install() {
+function pluginprefix_install()
+{
     //create Tables
     global $wpdb, $table_prefix;
     $tables = array(
-            $table_prefix . "tbGold",
-            $table_prefix . "tbSilber",
-            $table_prefix . "tbpalladium",
-            $table_prefix . "tbPlatin",
-        );
-    require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+        $table_prefix . "Gold",
+        $table_prefix . "Silver",
+        $table_prefix . "Palladium",
+        $table_prefix . "Platinum",
+    );
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     foreach ($tables as $table) {
         $create_table = "
 CREATE TABLE {$table} (
 id int(11) NOT NULL AUTO_INCREMENT,
-name varchar(512) COLLATE utf8_persian_ci NOT NULL,
-Akstufe1 double  NOT NULL,
-Akstufe2 double  NOT NULL,
-Akstufe3 double  NOT NULL,
-Akstufe4 double  NOT NULL,
-Vkstufe1 double  NOT NULL,
-Vkstufe2 double  NOT NULL,
-Vkstufe3 double  NOT NULL,
-Vkstufe4 double  NOT NULL,
+Name varchar(512) COLLATE utf8_persian_ci NOT NULL,
+BasicSale double  NOT NULL,
+AvancedSale double  NOT NULL,
+SuperiorSale double  NOT NULL,
+InstutionalSale double  NOT NULL,
+BasicPurchase double  NOT NULL,
+AvancedPurchase double  NOT NULL,
+SuperiorPurchase double  NOT NULL,
+InstutionalPurchase double  NOT NULL,
 Date TIMESTAMP NOT NULL DEFAULT CURRENT_DATE(),
 PRIMARY KEY (id)
 ) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-" ;
+";
         dbDelta($create_table);
     }
     //cronjob run
-    if(!wp_next_scheduled('scheduled_product_tasks')) {
-        wp_schedule_event(time(),'products_five_seconds','scheduled_product_tasks');
+    if (!wp_next_scheduled('scheduled_product_tasks')) {
+        wp_schedule_event(time(), 'products_five_seconds', 'scheduled_product_tasks');
     }
 
 }
-register_activation_hook( __FILE__, 'pluginprefix_install' );
-function pluginprefix_deactivation() {
+
+register_activation_hook(__FILE__, 'pluginprefix_install');
+function pluginprefix_deactivation()
+{
     // To remove the table
     global $wpdb, $table_prefix;
     $tables = array(
-        $table_prefix . "tbGold",
-        $table_prefix . "tbSilber",
-        $table_prefix . "tbpalladium",
-        $table_prefix . "tbPlatin",
+        $table_prefix . "Gold",
+        $table_prefix . "Silver",
+        $table_prefix . "Palladium",
+        $table_prefix . "Platinum",
     );
     foreach ($tables as $table) {
         $sql = "DROP TABLE IF EXISTS $table";
@@ -66,20 +69,29 @@ function pluginprefix_deactivation() {
     //deactive cronjob
     wp_clear_scheduled_hook('scheduled_product_tasks');
 }
-register_deactivation_hook( __FILE__, 'pluginprefix_deactivation' );
 
+register_deactivation_hook(__FILE__, 'pluginprefix_deactivation');
 
-function add_kaufpreise($attr,$content=null) {
+function prices_load_scripts()
+{
+    wp_enqueue_style('lotmgr_map_style', plugins_url('/css/style.css', __FILE__));
+}
+
+add_action('wp_enqueue_scripts', 'prices_load_scripts');
+
+function add_kaufpreise($attr, $content = null)
+{
     global $wpdb, $table_prefix;
-    $table_gold = $table_prefix . "tbGold";
-    $table_silber = $table_prefix . "tbSilber";
-    $table_palladium= $table_prefix . "tbpalladium";
-    $table_platin = $table_prefix . "tbPlatin";
+    $table_gold = $table_prefix . "Gold";
+    $table_silber = $table_prefix . "Silver";
+    $table_palladium = $table_prefix . "Palladium";
+    $table_platin = $table_prefix . "Platinum";
     $option = shortcode_atts([
         'metal' => '',
         'value' => '',
-        'kaufstatus' => ''
-    ],$attr);
+        'status' => '',
+        'lng' => '',
+    ], $attr);
     switch ($option['metal']) {
         case 'gold' :
             $result = $wpdb->get_results("SELECT * FROM $table_gold ");
@@ -95,58 +107,87 @@ function add_kaufpreise($attr,$content=null) {
             break;
     }
     ob_start();
+    $res = $wpdb->get_row("SELECT Date FROM $table_gold ORDER BY id ASC LIMIT 1");
+    $timestamp = $res->Date;
+
+    if ($option['status'] == 'Kaufpreis' && $option['lng'] == 'EN')
+        $title = 'Sale price';
+    elseif ($option['status'] == 'Verkaufpreis' && $option['lng'] == 'EN')
+        $title = 'Purchase price';
+    else
+        $title = $option['status'];
     ?>
     <table style="width:100%">
         <tr>
-            <th style="font-family: 'Montserrat'; font-size:18px; letter-spacing: 1px;font-weight: 600;"><?=$option['kaufstatus'] ?>*</th>
+            <th class="custom-common custom-header "><?= $title; ?>
+                *
+            </th>
             <th></th>
         </tr>
         <tr>
-            <td style="font-family: 'Montserrat'; font-size:18px; letter-spacing: 1px;font-weight: 300;font-style: italic; color:#969BB4" >stand: <?=date("Y-m-d H:i:s");?></td>
-            <td style="font-family: 'Montserrat'; font-size:18px; letter-spacing: 1px;font-weight: 300;font-style: italic; color:#969BB4" >55.81 chf /g</td>
+            <td class="custom-common custom-cell">
+                <?php
+                if ($option['lng'] == 'EN')
+                    $date = date("d/m/Y H:i", strtotime($timestamp));
+                else
+                    $date = date("d.m.Y H:i", strtotime($timestamp)) . " Uhr";
+                ?>
+                stand: <?= $date ?></td>
+            <td class="custom-common custom-cell">
+                55.81 chf /g
+            </td>
         </tr>
         <?php foreach ($result as $row) {
             switch (true) {
-                case $option['value']=='standard' && $option['kaufstatus']=='kaufpreis' :
-                    $stufe=$row->Akstufe1;
+                case $option['value'] == 'standard' && $option['status'] == 'Kaufpreis' :
+                    $stufe = $row->BasicPurchase;
                     break;
-                case $option['value']=='standard' && $option['kaufstatus']=='verkaufpreis' :
-                    $stufe=$row->Vkstufe1;
+                case $option['value'] == 'standard' && $option['status'] == 'Verkaufpreis' :
+                    $stufe = $row->BasicSale;
                     break;
-                case  $option['value']=='advanced' && $option['kaufstatus']=='kaufpreis':
-                    $stufe=$row->Akstufe2;
+                case  $option['value'] == 'advanced' && $option['status'] == 'Kaufpreis':
+                    $stufe = $row->AvancedPurchase;
                     break;
-                case  $option['value']=='advanced' && $option['kaufstatus']=='verkaufpreis':
-                    $stufe=$row->Vkstufe2;
+                case  $option['value'] == 'advanced' && $option['status'] == 'Verkaufpreis':
+                    $stufe = $row->AvancedSale;
                     break;
-                case $option['value']=='superior' && $option['kaufstatus']=='kaufpreis':
-                    $stufe=$row->Akstufe3;
+                case $option['value'] == 'superior' && $option['status'] == 'Kaufpreis':
+                    $stufe = $row->SuperiorPurchase;
                     break;
-                case $option['value']=='superior' && $option['kaufstatus']=='verkaufpreis':
-                    $stufe=$row->Vkstufe3;
+                case $option['value'] == 'superior' && $option['status'] == 'Verkaufpreis':
+                    $stufe = $row->SuperiorSale;
                     break;
-                case $option['value']=='institutional' && $option['kaufstatus']=='kaufpreis':
-                    $stufe=$row->Akstufe4;
+                case $option['value'] == 'institutional' && $option['status'] == 'Kaufpreis':
+                    $stufe = $row->InstutionalPurchase;
                     break;
-                case $option['value']=='institutional' && $option['kaufstatus']=='verkaufpreis':
-                    $stufe=$row->Vkstufe4;
+                case $option['value'] == 'institutional' && $option['status'] == 'Verkaufpreis':
+                    $stufe = $row->InstutionalSale;
                     break;
             }
             ?>
             <tr>
-                <td style="font-family: 'Montserrat'; font-size:18px; letter-spacing: 1px;font-weight: 300;font-style: italic;"><?php echo $row->name; ?>CHF</td>
-                <td style="font-family: 'Montserrat'; font-size:18px; letter-spacing: 1px;font-weight: 300;font-style: italic;"><?php echo $stufe; ?>CHF</td>
+                <td class="custom-common  custom-cell-name">
+                    <?php if ($option['metal'] == 'silber' && $option['lng'] == 'EN')
+                        echo str_replace('Silber', 'Silver', $row->Name);
+                    elseif ($option['metal'] == 'platin' && $option['lng'] == 'EN')
+                        echo str_replace('Platin', 'Platinum', $row->Name);
+                    else echo $row->Name; ?>
+                </td>
+                <td class="custom-common  custom-cell-name"><?php echo $stufe; ?>
+                    CHF
+                </td>
             </tr>
         <?php } ?>
     </table>
     <?php
     return ob_get_clean();
 }
-add_shortcode('display_kaufprices', 'add_kaufpreise');
+
+add_shortcode('Preisliste', 'add_kaufpreise');
 ?>
 <?php
 //create cronjob
-add_filter('cron_schedules','cron_price');
+/*add_filter('cron_schedules','cron_price');
 function cron_price ($schedules) {
     if(!isset($schedules["products_five_seconds"])) {
         $schedules['products_five_seconds'] = array(
@@ -160,4 +201,5 @@ add_action('scheduled_product_tasks','product_to_sync');
 function product_to_sync() {
     echo "set cronjob";
 }
+*/
 //add_action('init',"read_db");
